44bb072017a0e2ccea767b7c6f21322f0a7f6879
josemaridorado02 <josemariaoteodorado12@gmail.com>
2025-05-19 13:22:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def detalle_juego(id):
     return render_template('detalle_juego.html', juego=juego)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
